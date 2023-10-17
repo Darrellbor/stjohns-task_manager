1c8591f64a61e76fd6eb3c037db567b6738edfb7
Darrel Idiagbor <idiagbordarrel@gmail.com>
2023-10-17 10:27:47 +0200

    chore: adjustments to jest setup tests file

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+//storybook
+// import { setProjectAnnotations } from '@storybook/testing-react';
+// import * as globalStorybookConfig from '../.storybook/preview'; // path of your preview.js file
+
+// setProjectAnnotations(globalStorybookConfig as any);
