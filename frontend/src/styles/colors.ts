0cf13d666b0042cb19729a8a3129b6e47df73382
Darrel Idiagbor <idiagbordarrel@gmail.com>
2023-10-17 10:14:33 +0200

    feat: colors implemented

---
+++ frontend/src/styles/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+    calendar: {
+        today: "#47A7FF",
+        available: "#2DBA21",
+        filled: "#C32323", 
+    },
+    white: "#ffffff",
+    backdrop: "#112259",
+    transparent: "transparent"
+}
+
+export default colors;
